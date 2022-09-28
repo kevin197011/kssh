f4851370d35914272f4d0a8cfd3c80fd56b0b5d4
kevin <kevin197011.l@gmail.com>
2022-09-28 21:56:37 +0800

    update.

---
+++ Rakefile
@@ -0,0 +1,7 @@
+task default: [:run]
+
+task :run do
+  sh 'git add .'
+  sh 'git commit -m "update."'
+  sh 'git push -u origin main'
+end
---
+++ config.rb
@@ -0,0 +1,7 @@
+# config host list
+@kssh_hosts = [
+  { 'name': 'devops-node1', 'ip': '1.2.3.1', 'username': 'root', 'method': 'key', 'passwdOrKey': 'id_rsa' },
+  { 'name': 'devops-node2', 'ip': '1.2.3.2', 'username': 'root', 'method': 'key', 'passwdOrKey': 'id_rsa' },
+  { 'name': 'devops-node3', 'ip': '1.2.3.3', 'username': 'root', 'method': 'key', 'passwdOrKey': 'id_rsa' }
+]
+
